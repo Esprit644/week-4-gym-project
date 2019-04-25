ba4b3ecc03f92f056739d43268f9d4d53b8f4b09
Peter Bisset <peter.bisset@hotmail.co.uk>
2019-04-25 16:47:36 +0100

    set up db schema

---
+++ gym/db/gym.sql
@@ -0,0 +1,25 @@
+DROP TABLE bookings;
+DROP TABLE members;
+DROP TABLE classes;
+
+CREATE TABLE members(
+id SERIAL8 PRIMARY KEY,
+first_name VARCHAR(255),
+last_name VARCHAR(255),
+phone_number INT2,
+email VARCHAR(255)
+);
+
+CREATE TABLE classes(
+id SERIAL8 PRIMARY KEY,
+class_name VARCHAR(255),
+cost INT2,
+class_time INT2,
+capacity INT2
+);
+
+CREATE TABLE bookings(
+id SERIAL8 PRIMARY KEY,
+member_id INT8 REFERENCES members(id) ON CASCADE DELETE,
+class_id INT8 REFERENCES classes(id) ON CASCADE DELETE,
+);
