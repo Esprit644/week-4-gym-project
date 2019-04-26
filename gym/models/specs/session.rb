39b829e7d4203d48b45acba825e0230c2b40120c
Peter Bisset <peter.bisset@hotmail.co.uk>
2019-04-26 09:46:02 +0100

    added session

---
+++ gym/models/specs/session.rb
@@ -0,0 +1,15 @@
+require('pry')
+require_relative ('../db/sql_runner.rb')
+
+class Session
+
+  attr_reader (:id)
+  attr_accessor(:session_name, :capacity, :cost, :session_time)
+
+  def initialize(options)
+    @id = options['id'].to_i
+    @session_name = options['session_name']
+    @capacity = options.['capacity'].to_i
+    @cost = options.['cost'].to_i
+    @session_time = options.['session_time'].to_i
+  end
