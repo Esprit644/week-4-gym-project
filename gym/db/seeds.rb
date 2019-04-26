b4684f205e3fd0c3c4e3685b00c42b6154b137f4
Peter Bisset <peter.bisset@hotmail.co.uk>
2019-04-26 16:51:33 +0100

    sessions table saving data

--- gym/db/seeds.rb
+++ gym/db/seeds.rb
@@ -30,10 +30,15 @@ session2.save()
 session3.save()
 Session.show()
 Session.find(session2.id)
-session3.delete_session()
-#
-# booking1 = Booking.new({'member_id' => member1.id, 'session_id' => session1.id})
-# booking2 = Booking.new({'member_id' => member2.id, 'session_id' => session1.id})
-#
-# booking3 = Booking.new({'member_id' => member3.id, 'session_id' => session2.id)}
-# booking4 = Booking.new({'member_id' => member4.id, 'session_id' => session3.id})
+# session3.delete_session()
+
+booking1 = Booking.new({'member_id' => member1.id, 'session_id' => session1.id})
+booking2 = Booking.new({'member_id' => member2.id, 'session_id' => session1.id})
+
+booking3 = Booking.new({'member_id' => member3.id, 'session_id' => session2.id})
+booking4 = Booking.new({'member_id' => member4.id, 'session_id' => session3.id})
+
+booking1.save()
+booking2.save()
+booking3.save()
+booking4.save()
--- gym/models/booking.rb
+++ gym/models/booking.rb
@@ -10,4 +10,19 @@ class Booking
     @session_id = options['session_id'].to_i
   end
 
+  def save()
+    sql = "INSERT INTO bookings (
+    member_id,
+    session_id
+    )
+    VALUES (
+      $1,
+      $2)
+      RETURNING id"
+      values = [@member_id, @session_id]
+    result = SqlRunner.run(sql, values)
+    id = result.first['id']
+    @id = id.to_i
+  end
+
 end
