8471999baacb86dab84c539600975b4a56e78c00
Peter Bisset <peter.bisset@hotmail.co.uk>
2019-05-01 11:22:26 +0100

    files moved into appropriate folders

--- gym/gym_controller.rb
+++ gym/gym_controller.rb
@@ -25,42 +25,42 @@ end
 
 
 # show
-get '/show_member' do
+get '/members/show_member' do
   @member = Member.find_all()
-  erb(:show_member)
+  erb(:"/members/show_member")
 end
 
 # new
-get '/new_member' do
-  erb(:new_member)
+get '/members/new_member' do
+  erb(:"/members/new_member")
 end
 
 # create
-post '/new_member' do
+post '/members/new_member' do
   @member = Member.new(params)
   @member.save()
-  redirect to '/show_member'
+  redirect to '/members/show_member'
 end
 
 # edit
-get '/edit_member/:id/edit' do
+get '/members/edit_member/:id/edit' do
   @member = Member.find(params[:id])
-  erb(:edit_member)
+  erb(:"/members/edit_member")
 end
 
 # update
-post '/edit_member/:id' do
+post '/members/edit_member/:id' do
   Member.new(params).update
-  redirect to '/show_member'
+  redirect to '/members/show_member'
 end
 
 # destroy
 
-get '/show_member/:id/delete' do
+get '/members/show_member/:id/delete' do
   @member = Member.find(params[:id]).delete()
 
 
-  redirect to '/show_member'
+  redirect to '/members/show_member'
 
 end
 
@@ -68,53 +68,51 @@ end
 
 # SESSION*****************************************
 # new
-get '/new_session' do
-  erb(:new_session)
+get '/sessions/new_session' do
+  erb(:"/sessions/new_session")
 end
 
 # create
-post '/new_session' do
+post '/sessions/new_session' do
   @session = Session.new(params)
   @session.save()
-  redirect to '/show_session'
+  redirect to '/sessions/show_session'
 end
 
 # show
-get '/show_session' do
+get '/sessions/show_session' do
   @session = Session.show()
-  erb(:show_session)
+  erb(:"/sessions/show_session")
 end
 
-get '/show_attendance/:id' do
+get '/sessions/show_attendance/:id' do
 
   @session = Session.find_session(params[:id])
   @members = @session.members()
-  # @sessions = Session.show()
-  erb(:show_attendance)
+  erb(:"/sessions/show_attendance")
 end
 
-get '/time_session' do
+get '/sessions/time_session' do
   @session = Session.sessions_by_time()
-  erb(:time_session)
+  erb(:"/sessions/time_session")
 end
 
 # edit
-get '/edit_session/:id/edit' do
+get '/sessions/edit_session/:id/edit' do
   @sessions = Session.find_session(params[:id])
-  # binding.pry
-erb(:edit_session)
+erb(:"/sessions/edit_session")
 end
 
 # update
-post '/edit_session/:id' do
+post '/sessions/edit_session/:id' do
  Session.new(params).update
-  redirect to '/show_session'
+  redirect to '/sessions/show_session'
 end
 
 # destroy
-get '/show_session/:id/delete' do
+get '/sessions/show_session/:id/delete' do
   @sessions = Session.find_session(params[:id]).delete_session()
-redirect to '/show_session'
+redirect to '/sessions/show_session'
 end
 
 
@@ -124,50 +122,44 @@ end
 # bookings***********************************************
 
 # new
-get '/new_booking' do
+get '/bookings/new_booking' do
   @sessions = Session.show()
   @members = Member.find_all()
-erb(:new_booking)
+erb(:"/bookings/new_booking")
 end
 
 # create
-post '/new_booking' do
+post '/bookings/new_booking' do
   @booking = Booking.new(params)
   @booking.save()
-  redirect to '/show_booking'
-  erb(:new_booking)
+  redirect to '/bookings/show_booking'
+  erb(:"/bookings/new_booking")
 end
 
 # show
-get '/show_booking' do
+get '/bookings/show_booking' do
    @booking = Booking.show()
-   erb(:show_booking)
+   erb(:"/bookings/show_booking")
 end
 
-
-
-
-
 # edit
-get '/edit_booking/:id/edit' do
+get '/bookings/edit_booking/:id/edit' do
    @booking = Booking.find(params[:id])
    @sessions = Session.show()
    @members = Member.find_all()
-   # binding.pry
-   # nil
-erb(:edit_booking)
+erb(:"/bookings/edit_booking")
 end
 
 # update
-post '/edit_booking/:id' do
+post '/bookings/edit_booking/:id' do
   @booking = Booking.new(params).update()
-  redirect to '/show_booking'
+  redirect to '/bookings/show_booking'
 end
 
 # destroy
 
-get '/show_booking/:id/delete' do
+get '/bookings/show_booking/:id/delete' do
   @booking = Booking.find(params[:id]).delete_booking()
   # @booking.delete_booking()
-  redirect to '/show_booking'
+  redirect to '/bookings/show_booking'
 end
